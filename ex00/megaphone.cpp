08c4ce524d1c963df17acda6855cbdb3a72bd685
Ayoub <aybelhaj@cbr3s3.42barcelona.com>
2025-10-14 19:40:43 +0200

    add ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main(int argc, char **argv)
+{
+	if(argc == 1)
+		std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *"<<std::endl;
+	else
+	{
+		for(int i=1;i<argc;i++)
+		{
+			for(int j=0;argv[i][j];j++)
+			{
+				char c=toupper(argv[i][j]);
+				std::cout<<c;
+			}
+		}
+		std::cout<<std::endl;
+	}
+}
